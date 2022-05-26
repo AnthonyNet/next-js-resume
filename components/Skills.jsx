ba6e6a5309013cba5cc23015d87a91f075e9eeab
AnthonyZet <zejda.anthony@gmai.com>
2022-05-26 20:00:33 +0200

    vercel images problem

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react';
+import Html from '/../public/assets/skills/html.png';
 
 const Skills = () => {
   return (
@@ -15,7 +16,7 @@ const Skills = () => {
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div className='m-auto'>
                 <Image
-                  src='/../public/assets/skills/html.png'
+                  src={Html}
                   width='64px'
                   height='64px'
                   alt='/'
