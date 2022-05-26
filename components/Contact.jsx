6ffac98b0941deb5c73a9e49bf36cc6b47c0ba64
AnthonyZet <zejda.anthony@gmai.com>
2022-05-26 16:46:09 +0200

    first upload

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
     <span className="text-2xl font-light">Let me know</span>
     <div className="mt-4 bg-white shadow-md rounded-lg">
       <div className="h2 bg-indigo-400 rounded-t-m py-2"></div>
-      <div className="px-8 py-6">
+      <div className="px-8">
         <label  className="block font-semibold">Your email</label>
-        <input type="text" placeholder="Email" class="border w-full h-5 py-6" />
+        <input type="text" placeholder="Email" className="border w-full h-5 py-6" />
         <textarea className='w-full py-6 border' placeholder="Your questions here"></textarea>
         <div className='flex justify-between items-baseline'>
           <button className="mt-4 bg-indigo-500 text-white p-3 hover:scale-105 ease-in duration-300" type="submit">Send question</button>
